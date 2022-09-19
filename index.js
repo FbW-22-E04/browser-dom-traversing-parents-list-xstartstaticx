000948903136395add5e6c07438b6485f24e60d1
xstartstaticx <daniel.reiller@icloud.com>
2022-09-19 15:39:16 +0200

    removed duplicate code

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ const parentList = [];
 
 let currentElement = document.querySelector("cite");
 
-let currentElementName = currentElement.nodeName;
-
-parentList.unshift(currentElementName.toLowerCase());
-
-currentElement = currentElement.parentElement;
-
 while (currentElement) {
   let currentElementName = currentElement.nodeName;
   parentList.unshift(currentElementName.toLowerCase());
